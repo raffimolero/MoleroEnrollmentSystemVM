bbe9348619b0224dbe57c553e54e29ada9145f15
Raffi Molero <raffimolero@gmail.com>
2025-11-07 12:37:05 +0800

    hmm yes my code (from fxcatalan with his permissions)

--- html/evaluate.php
+++ html/evaluate.php
@@ -1,3 +1,8 @@
 <?php
 
-print 'hello world';
+// Database configuration
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+
+list($database, $student_id, $subject_id) = explode(':', base64_decode($_GET['params']));
